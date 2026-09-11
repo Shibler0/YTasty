d73984a2eef875457cca11d55515b1f6a65e7573
Shibler0 <rischmartin@laposte.net>
2026-09-11 16:47:31 +0200

    chore:manage errors

--- src/ytasty/main.py
+++ src/ytasty/main.py
@@ -10,6 +10,15 @@ from ytasty.modules.restaurants.model import Restaurant
 from ytasty.modules.restaurants.router import router as restaurants_router
 from ytasty.modules.users.model import User
 from ytasty.modules.users.router import router as users_router
+from fastapi import FastAPI, Request
+from fastapi.responses import JSONResponse
+
+from ytasty.common.errors import (
+    BadRequestError,
+    ForbiddenError,
+    NotFoundError,
+    UnauthorizedError,
+)
 
 
 MODELS = [
@@ -26,6 +35,38 @@ app = FastAPI(
     title="Ytasty Crousty API",
     version="1.0.0",
 )
+
+@app.exception_handler(NotFoundError)
+def not_found_handler(request: Request, error: NotFoundError):
+    return JSONResponse(
+        status_code=404,
+        content={"detail": error.message},
+    )
+
+
+@app.exception_handler(UnauthorizedError)
+def unauthorized_handler(request: Request, error: UnauthorizedError):
+    return JSONResponse(
+        status_code=401,
+        content={"detail": error.message},
+    )
+
+
+@app.exception_handler(ForbiddenError)
+def forbidden_handler(request: Request, error: ForbiddenError):
+    return JSONResponse(
+        status_code=403,
+        content={"detail": error.message},
+    )
+
+
+@app.exception_handler(BadRequestError)
+def bad_request_handler(request: Request, error: BadRequestError):
+    return JSONResponse(
+        status_code=400,
+        content={"detail": error.message},
+    )
+
 
 app.include_router(auth_router)
 app.include_router(users_router)
