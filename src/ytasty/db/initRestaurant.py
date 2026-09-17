bc910562eecb929ebba8914b68d8b2b59fef683c
Shibler0 <rischmartin@laposte.net>
2026-09-17 14:34:47 +0200

    new branch

---
+++ src/ytasty/db/initRestaurant.py
@@ -0,0 +1,27 @@
+def seed_restaurants(db):
+    restaurants = [
+        {
+            "name": "Ytasty Crousty Aix",
+            "city": "Aix-en-Provence",
+            "address": "Adresse à définir",
+            "is_open": True,
+            "opening_hours": "À définir",
+            "contact": "À définir",
+        },
+        {
+            "name": "Ytasty Crousty Lyon",
+            "city": "Lyon",
+            "address": "Adresse à définir",
+            "is_open": True,
+            "opening_hours": "À définir",
+            "contact": "À définir",
+        },
+        {
+            "name": "Ytasty Crousty Paris",
+            "city": "Paris",
+            "address": "Adresse à définir",
+            "is_open": True,
+            "opening_hours": "À définir",
+            "contact": "À définir",
+        },
+    ]
